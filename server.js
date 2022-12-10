8df3be9dc76511d01e08d3d4df9e2ec60f8d3eb8
Nadir Hussain <nadirhussain1@gmail.com>
2022-12-10 13:49:47 +0500

    streaming a video through nodejs pipe

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
-    res.send('Hello World');
+app.get('/video', (req, res) => {
+    const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            console.error("Error while streaming application=",err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+
+        fs.createReadStream(path).pipe(res);
+
+    })
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server listening on port 3000");
 })
