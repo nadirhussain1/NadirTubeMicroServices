3708e0ce655fb1c56758a6870782378177d73be8
Nadir Hussain <nadirhussain1@gmail.com>
2023-01-07 19:45:18 +0500

    updated video-streaming microservice to use storage service for video file related stuff

--- video-streaming/src/server.js
+++ video-streaming/src/server.js
@@ -1,31 +1,28 @@
 import express from 'express';
-import fs from 'fs';
+import http from 'http';
 
 const app = express();
 
-if (!process.env.PORT) {
-    throw new Error("Please specify the port number");
-}
-
 const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST=process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT=parseInt(rocess.env.VIDEO_STORAGE_PORT);
 
 app.get('/video', (req, res) => {
-    const path = "./videos/SampleVideo_1280x720_1mb.mp4";
-    fs.stat(path, (err, stats) => {
-        if (err) {
-            console.error("Error while streaming application=",err);
-            res.sendStatus(500);
-            return;
+    const forwardRequest = http.request(
+        {
+          host: VIDEO_STORAGE_HOST,
+          port: VIDEO_STORAGE_PORT,
+          path:'/video?path=SampleVideo_1280x720_1mb.mp4', 
+          method: 'GET',
+          headers: req.headers
+        },
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode,forwardResponse.headers);
+            forwardResponse.pipe(res);
         }
+    );
 
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
-        });
-
-        fs.createReadStream(path).pipe(res);
-
-    })
+    req.pipe(forwardRequest); 
 })
 
 app.listen(PORT, () => {
